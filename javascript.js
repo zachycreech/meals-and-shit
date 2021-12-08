553cd7c4333f34554ae586652a70c29a04af771f
Zach <zwheele1@gmail.com>
2021-12-08 13:52:37 -0500

    finalized

--- javascript.js
+++ javascript.js
@@ -1,15 +1,20 @@
 // global variables
-
-const foodTitle = $("#foodTitle")
-const drinkTitle = $("#drinkTitle")
-const foodDescr = $("#foodDescription")
-const drinkDescr = $("#drinkDescription")
-const foodGroup = $("#foodGroup")
-const drinkGroup = $("#drinkGroup")
 const searchButton = $("#searchButton")
-const foodImg = $("#foodImg")
-const drinkImg = $("#drinkImage")
 const apiKey = "45fa1958465544f1a8f06e376252e2a6"
+
+//food variables
+const foodTitle = $("#foodTitle")
+const foodImg = $("#foodImg")
+const foodIngredients = $(".foodIngredients")
+const foodDescr = $("#foodDescription")
+const foodGroup = $("#foodGroup")
+
+// drink variables
+const drinkTitle = $("#drinkTitle")
+const drinkImg = $("#drinkImage")
+const drinkIngredients = $(".drinkIngredients")
+const drinkDescr = $("#drinkDescription")
+const drinkGroup = $("#drinkGroup")
 
 //hide images on load
 
@@ -53,30 +58,34 @@ const getRecipe = async function () {
     foodGroupId = $(foodGroup).find("button.btn-primary").attr("id")
   }
   const inputText = $("#foodInput").val().split(",")
-  const ingredients = []
+  const ingredientsQuery = []
   for (let i = 0; i < inputText.length; i++) {
     const singleItem = inputText[i].trim()
-    ingredients.push("+" + singleItem)
+    ingredientsQuery.push("+" + singleItem)
   }
-  ingredients.unshift(foodGroupId)
+  ingredientsQuery.unshift(foodGroupId)
 
   let response = await fetch(
-    `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=1&apiKey=${apiKey}`
+    `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientsQuery}&number=1&apiKey=${apiKey}`
   )
   let data = await response.json()
   const { title, image, id } = data[0]
 
-  console.log(id)
   let secondResponse = await fetch(
     `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`
   )
   let secondData = await secondResponse.json()
-  const { instructions, summary } = secondData
+  const { instructions, summary, extendedIngredients } = secondData
 
-  postRecipe(title, image, instructions, summary)
+  let ingredients = []
+  for (let i = 0; i < extendedIngredients.length; i++) {
+    ingredients.push(extendedIngredients[i].original)
+  }
+
+  postRecipe(title, image, instructions, summary, ingredients)
 }
 
-const postRecipe = function (title, image, instructions, summary) {
+const postRecipe = function (title, image, instructions, summary, ingredients) {
   foodTitle.html(title)
   foodImg.attr("src", image)
   foodImg.removeClass("hidden")
@@ -85,7 +94,10 @@ const postRecipe = function (title, image, instructions, summary) {
   } else {
     foodDescr.html(instructions)
   }
-  console.log(summary)
+  foodIngredients.empty()
+  for (let i = 0; i < ingredients.length; i++) {
+    foodIngredients.append(`<li>${ingredients[i]}</li>`)
+  }
 }
 
 // start the get request after search has been pressed
@@ -113,12 +125,28 @@ const getDrink = async function () {
 
   const { strInstructions: instructions } = secondData.drinks[0]
 
-  postDrink(title, image, instructions)
+  let ingredients = []
+  for (let i = 0; i < 15; i++) {
+    const measurement = eval(`secondData.drinks[0].strMeasure${i}`)
+    const ingredient = eval(`secondData.drinks[0].strIngredient${i}`)
+    if (measurement && ingredient) {
+      ingredients.push(`${measurement} ${ingredient}`)
+    }
+    if (!measurement && ingredient) {
+      ingredients.push(ingredient)
+    }
+  }
+
+  postDrink(title, image, instructions, ingredients)
 }
 
-const postDrink = function (title, image, instructions) {
+const postDrink = function (title, image, instructions, ingredients) {
   drinkTitle.html(title)
   drinkImg.attr("src", image)
   drinkImg.removeClass("hidden")
   drinkDescr.html(instructions)
+  drinkIngredients.empty()
+  for (let i = 0; i < ingredients.length; i++) {
+    drinkIngredients.append(`<li>${ingredients[i]}</li>`)
+  }
 }
